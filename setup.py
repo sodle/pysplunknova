abf05ef808cc0b290c15e28c664a8687034a06e5
Scott Odle <sjodle@asu.edu>
2018-02-19 09:54:50 -0700

    init project

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='splunknova',
+      version='1.0',
+      description='Python Client for Splunk Project Nova',
+      author='Scott Odle',
+      author_email='scott@sjodle.com',
+      url='https://github.com/sodle/pysplunknova',
+      packages=['splunknova']
+      )
