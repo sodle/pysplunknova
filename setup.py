635a9370503c1665ea09d22d6dc905019dda169e
Scott Odle <sjodle@asu.edu>
2018-02-20 11:11:29 -0700

    download url

--- setup.py
+++ setup.py
@@ -8,5 +8,6 @@ setup(name='splunknova',
       author='Scott Odle',
       author_email='scott@sjodle.com',
       url='https://github.com/sodle/pysplunknova',
+      download_url='https://github.com/sodle/pysplunknova/archive/1.0.0.tar.gz',
       packages=['splunknova']
       )
