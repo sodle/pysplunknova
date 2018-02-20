6a217706e9041c76043a2e6c75a88e02be44e040
Scott Odle <sjodle@asu.edu>
2018-02-20 12:22:17 -0700

    increment version

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from distutils.core import setup
 
 setup(name='splunknova',
-      version='1.0.1',
+      version='1.0.2',
       description='Python Client for Splunk Project Nova',
       author='Scott Odle',
       author_email='scott@sjodle.com',
       url='https://github.com/sodle/pysplunknova',
-      download_url='https://github.com/sodle/pysplunknova/archive/1.0.1.tar.gz',
+      download_url='https://github.com/sodle/pysplunknova/archive/1.0.2.tar.gz',
       license='GPL v3',
       packages=['splunknova']
       )
